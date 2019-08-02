c2d0f975bd2fa27ad14ee5ffd4988216665fefe9
raunakchowdhury <rchowdhury5@stuy.edu>
2019-08-02 15:15:21 -0400

    added 404 errorhandler

--- index.js
+++ index.js
@@ -45,3 +45,7 @@ app.get("/apply/:position", (req, res) => {
 });
 
 app.listen(port, () => console.log(`App running on localhost:${port}`));
+
+app.use((req, res, next) => {
+  res.status(404).render("404.html");
+});
