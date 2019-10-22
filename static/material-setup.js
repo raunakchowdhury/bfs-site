bf557c866fddd3bec515378d7a4b45ef7d3450b6
Abir Taheer <abir488@gmail.com>
2019-10-22 01:29:18 -0400

    made some progress on material

---
+++ static/material-setup.js
@@ -0,0 +1,40 @@
+let initializeItems = (selector, mdcClass) => {
+  let stuff = document.querySelectorAll(selector);
+  for( let x = 0 ; x < stuff.length ; x ++ ){
+    mdcClass.attachTo(stuff[x]);
+  }
+};
+
+let initializeMDC = (alreadyCalled = false) => {
+  // Handle stuff like buttons that should automatically have ripple added
+
+  initializeItems(".mdc-button, .mdc-tab__ripple, .mdc-card__primary-action", mdc.ripple.MDCRipple);
+
+  initializeItems(".mdc-text-field", mdc.textField.MDCTextField);
+
+  initializeItems(".mdc-text-field-helper-text", mdc.textField.MDCTextFieldHelperText);
+
+  initializeItems(".mdc-tab-bar", mdc.tabBar.MDCTabBar);
+
+  initializeItems(".mdc-list", mdc.list.MDCList);
+
+  initializeItems(".mdc-chip-set", mdc.chips.MDCChipSet);
+
+  initializeItems(".mdc-select", mdc.select.MDCSelect);
+
+  initializeItems(".mdc-text-field-character-counter", mdc.textField.MDCTextFieldCharacterCounter);
+
+  initializeItems(".mdc-radio", mdc.radio.MDCRadio);
+
+  alreadyCalled ? window.mdc.autoInit(document, () => {}) : mdc.autoInit();
+
+};
+
+initializeMDC();
+
+const drawer = new mdc.drawer.MDCDrawer(document.querySelector(".mdc-drawer"));
+
+document.querySelector(".mdc-top-app-bar__navigation-icon")
+    .addEventListener("click", () => {
+      drawer.open = ! drawer.open;
+    });
