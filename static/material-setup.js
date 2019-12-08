9a2be1d0a879037c669783525e690994cc1bf892
ray. lee <rlee00@stuy.edu>
2019-12-07 20:01:14 -0500

    hoverable sidebar and more UX-friendly icons

--- static/material-setup.js
+++ static/material-setup.js
@@ -1,42 +1,55 @@
 let initializeItems = (selector, mdcClass) => {
-    let stuff = document.querySelectorAll(selector);
-    for (let x = 0; x < stuff.length; x++) {
-        mdcClass.attachTo(stuff[x]);
-    }
+  let stuff = document.querySelectorAll(selector);
+  for (let x = 0; x < stuff.length; x++) {
+    mdcClass.attachTo(stuff[x]);
+  }
 };
 
 let initializeMDC = (alreadyCalled = false) => {
-    // Handle stuff like buttons that should automatically have ripple added
+  // Handle stuff like buttons that should automatically have ripple added
 
-    initializeItems(".mdc-button, .mdc-tab__ripple, .mdc-card__primary-action", mdc.ripple.MDCRipple);
+  initializeItems(
+    ".mdc-button, .mdc-tab__ripple, .mdc-card__primary-action",
+    mdc.ripple.MDCRipple
+  );
 
-    initializeItems(".mdc-text-field", mdc.textField.MDCTextField);
+  initializeItems(".mdc-text-field", mdc.textField.MDCTextField);
 
-    initializeItems(".mdc-text-field-helper-text", mdc.textField.MDCTextFieldHelperText);
+  initializeItems(
+    ".mdc-text-field-helper-text",
+    mdc.textField.MDCTextFieldHelperText
+  );
 
-    initializeItems(".mdc-tab-bar", mdc.tabBar.MDCTabBar);
+  initializeItems(".mdc-tab-bar", mdc.tabBar.MDCTabBar);
 
-    initializeItems(".mdc-list", mdc.list.MDCList);
+  initializeItems(".mdc-list", mdc.list.MDCList);
 
-    initializeItems(".mdc-chip-set", mdc.chips.MDCChipSet);
+  initializeItems(".mdc-chip-set", mdc.chips.MDCChipSet);
 
-    initializeItems(".mdc-select", mdc.select.MDCSelect);
+  initializeItems(".mdc-select", mdc.select.MDCSelect);
 
-    initializeItems(".mdc-text-field-character-counter", mdc.textField.MDCTextFieldCharacterCounter);
+  initializeItems(
+    ".mdc-text-field-character-counter",
+    mdc.textField.MDCTextFieldCharacterCounter
+  );
 
-    initializeItems(".mdc-radio", mdc.radio.MDCRadio);
+  initializeItems(".mdc-radio", mdc.radio.MDCRadio);
 
-    alreadyCalled ? window.mdc.autoInit(document, () => {
-    }) : mdc.autoInit();
-
+  alreadyCalled ? window.mdc.autoInit(document, () => {}) : mdc.autoInit();
 };
 
 initializeMDC();
 
 const drawer = new mdc.drawer.MDCDrawer(document.querySelector(".mdc-drawer"));
 
-document.querySelector(".mdc-top-app-bar__navigation-icon")
-    .addEventListener("click", () => {
-        drawer.open = !drawer.open;
-    })
+document
+  .querySelector(".mdc-top-app-bar__navigation-icon")
+  .addEventListener("mouseover", () => {
+    drawer.open = !drawer.open;
+  });
 
+document
+  .querySelector(".mdc-drawer-scrim")
+  .addEventListener("mouseover", () => {
+    drawer.open = !drawer.open;
+  });
